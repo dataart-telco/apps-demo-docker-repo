e5591a3ba159bbfc8a5d78200bc8961802c0fc33
gennadiy.dubina <Gennadiy.Dubina@dataart.com>
2015-08-14 14:25:49 +0300

    update images name

--- build-local.sh
+++ build-local.sh
@@ -1,3 +1,3 @@
-docker build -t taddemo/main:0.0.1 ./demo-main
-docker build -t taddemo/conference:0.0.1 ./demo-conference
-docker build -t taddemo/advertising:0.0.1 ./demo-advertising
+docker build -t tads2015da/demo-main:0.0.1 ./demo-main
+docker build -t tads2015da/demo-conference:0.0.1 ./demo-conference
+docker build -t tads2015da/demo-advertising:0.0.1 ./demo-advertising
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -t taddemo/main:0.0.1 https://github.com/taddemo2015/vas-demo-docker-repo.git#:demo-main
-docker build -t taddemo/conference:0.0.1 https://github.com/taddemo2015/vas-demo-docker-repo.git#:demo-conference
-docker build -t taddemo/advertising:0.0.1 https://github.com/taddemo2015/vas-demo-docker-repo.git#:demo-advertising
+docker build -t tads2015da/demo-main:0.0.1 https://github.com/taddemo2015/vas-demo-docker-repo.git#:demo-main
+docker build -t tads2015da/demo-conference:0.0.1 https://github.com/taddemo2015/vas-demo-docker-repo.git#:demo-conference
+docker build -t tads2015da/demo-advertising:0.0.1 https://github.com/taddemo2015/vas-demo-docker-repo.git#:demo-advertising
