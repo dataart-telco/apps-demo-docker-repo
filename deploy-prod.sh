6fb065cc6a3ab2ac61ea0b07e67440bb96717d36
root <root@ip-172-20-0-9.ec2.internal>
2015-08-18 17:27:55 +0000

    fix deploy script

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,17 +3,21 @@ kubectl create -f kube-yml/prod/main-service.yml
 kubectl create -f kube-yml/prod/conference-service.yml
 kubectl create -f kube-yml/prod/advertising-service.yml
 
-mainHost = kubectl get service/main -o json | sed 's/hostName://'
-confereceHost = kubectl get service/conference -o json | sed 's/hostName://'
-adHost = kubectl get service/advertising -o json | sed 's/hostName://'
+mainHost=$(kubectl get service/main -o json | grep hostname | sed 's/.*: //' | sed -e 's/^"//'  -e 's/"$//')
+confereceHost=$(kubectl get service/conference -o json | grep hostname | sed 's/.*: //' | sed -e 's/^"//'  -e 's/"$//')
+adHost=$(kubectl get service/advertising -o json | grep hostname | sed 's/.*: //' | sed -e 's/^"//'  -e 's/"$//')
 
-sed 's/EXTERNAL_IP_VALUE/$mainHost/g' kube-yml/prod/main-controller.yml > ./work/main-controller.yml
-sed 's/EXTERNAL_IP_VALUE/$conferenceHost/g' kube-yml/prod/conference-controller.yml > ./work/conference-controller.yml
-sed 's/EXTERNAL_IP_VALUE/$adHost/g' kube-yml/prod/advertising-pod.yml > ./work/advertising-pod.yml
+mkdir work
 
-restCommService=$0
-sed -i 's/RESTCOMM_SERVICE_VALUE/$restCommService/g' ./work/main-controller.yml
-sed -i 's/RESTCOMM_SERVICE_VALUE/$restCommService/g' ./work/conference-controller.yml
-sed -i 's/RESTCOMM_SERVICE_VALUE/$restCommService/g' ./work/advertising-pod.yml
+sed "s/EXTERNAL_IP_VALUE/"$mainHost"/g" kube-yml/prod/main-controller.yml > ./work/main-controller.yml
+sed "s/EXTERNAL_IP_VALUE/"$conferenceHost"/g" kube-yml/prod/conference-controller.yml > ./work/conference-controller.yml
+sed "s/EXTERNAL_IP_VALUE/"$adHost"/g" kube-yml/prod/advertising-pod.yml > ./work/advertising-pod.yml
 
+restCommService=$1
+echo $restCommService
 
+sed -i "s/RESTCOMM_SERVICE_VALUE/"$restCommService"/g" ./work/main-controller.yml
+sed -i "s/RESTCOMM_SERVICE_VALUE/"$restCommService"/g" ./work/conference-controller.yml
+sed -i "s/RESTCOMM_SERVICE_VALUE/"$restCommService"/g" ./work/advertising-pod.yml
+
+echo 'end'
