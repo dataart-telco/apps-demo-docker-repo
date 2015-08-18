a8ff2f4dc5fd57889ada25bc6bd2a3598fd105de
Gennadiy Dubina <gdubina@dataart.com>
2015-08-18 17:40:19 +0000

    create controllers

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -7,6 +7,10 @@ mainHost=$(kubectl get service/main -o json | grep hostname | sed 's/.*: //' | s
 confereceHost=$(kubectl get service/conference -o json | grep hostname | sed 's/.*: //' | sed -e 's/^"//'  -e 's/"$//')
 adHost=$(kubectl get service/advertising -o json | grep hostname | sed 's/.*: //' | sed -e 's/^"//'  -e 's/"$//')
 
+echo "Main host: "$mainHost
+echo "Conference host: "$conferenceHost
+echo "Advertising host: "${adHost}
+
 mkdir work
 
 sed "s/EXTERNAL_IP_VALUE/"$mainHost"/g" kube-yml/prod/main-controller.yml > ./work/main-controller.yml
@@ -14,10 +18,14 @@ sed "s/EXTERNAL_IP_VALUE/"$conferenceHost"/g" kube-yml/prod/conference-controlle
 sed "s/EXTERNAL_IP_VALUE/"$adHost"/g" kube-yml/prod/advertising-pod.yml > ./work/advertising-pod.yml
 
 restCommService=$1
-echo $restCommService
+echo "RestComm API server: "$restCommService
 
 sed -i "s/RESTCOMM_SERVICE_VALUE/"$restCommService"/g" ./work/main-controller.yml
 sed -i "s/RESTCOMM_SERVICE_VALUE/"$restCommService"/g" ./work/conference-controller.yml
 sed -i "s/RESTCOMM_SERVICE_VALUE/"$restCommService"/g" ./work/advertising-pod.yml
+
+kubectl create -f kube-yml/prod/redis-controller.yml
+kubectl create -f ./work/main-controller.yml
+kubectl create -f ./work/conference-controller.yml
 
 echo 'end'
