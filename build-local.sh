8305b5662e237f75f9707897f6d6b1546968e1a8
Gennadiy Dubina <gdubina@dataart.com>
2015-08-19 13:30:24 +0300

    v.0.0.6 -> wait until web server is ready

--- build-local.sh
+++ build-local.sh
@@ -1,3 +1,3 @@
-docker build -t tads2015da/demo-main:0.0.5 ./demo-main
-docker build -t tads2015da/demo-conference:0.0.5 ./demo-conference
-docker build -t tads2015da/demo-advertising:0.0.5 ./demo-advertising
+docker build -t tads2015da/demo-main:0.0.6 ./demo-main
+docker build -t tads2015da/demo-conference:0.0.6 ./demo-conference
+docker build -t tads2015da/demo-advertising:0.0.6 ./demo-advertising
