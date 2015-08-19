a32add13a32cb062c49504978b247b615e36a35e
Gennadiy Dubina <gdubina@dataart.com>
2015-08-19 17:28:04 +0300

    use xml mime type, update version to v0.9

--- build-local.sh
+++ build-local.sh
@@ -1,3 +1,3 @@
-docker build -t tads2015da/demo-main:0.0.6 ./demo-main
-docker build -t tads2015da/demo-conference:0.0.6 ./demo-conference
-docker build -t tads2015da/demo-advertising:0.0.6 ./demo-advertising
+docker build -t tads2015da/demo-main:0.0.9 ./demo-main
+docker build -t tads2015da/demo-conference:0.0.9 ./demo-conference
+docker build -t tads2015da/demo-advertising:0.0.9 ./demo-advertising
