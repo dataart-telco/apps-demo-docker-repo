5223d0934c510757b07714085dac12a633689289
Gennadiy Dubina <gdubina@dataart.com>
2015-10-20 09:39:53 +0300

    add drop conference

--- build-local.sh
+++ build-local.sh
@@ -5,3 +5,4 @@ docker build -t tads2015da/conference-call:$VERSION ./conference-call
 #ocker build -t tads2015da/demo-advertising:0.0.9 ./demo-advertising
 docker build -t tads2015da/mailagent:$VERSION ./mailagent
 docker build -t tads2015da/sms-feedback:$VERSION ./sms-feedback
+docker build -t tads2015da/drop-conference:$VERSION ./drop-conference
--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ docker build -t tads2015da/conference-call:$VERSION https://github.com/taddemo20
 #ocker build -t tads2015da/demo-advertising:0.0.9 ./demo-advertising
 docker build -t tads2015da/mailagent:$VERSION https://github.com/taddemo2015/vas-demo-docker-repo.git#$GIT_VERSION:mailagent
 docker build -t tads2015da/sms-feedback:$VERSION https://github.com/taddemo2015/vas-demo-docker-repo.git#$GIT_VERSION:sms-feedback
+docker build -t tads2015da/drop-conference:$VERSION https://github.com/taddemo2015/vas-demo-docker-repo.git#$GIT_VERSION:drop-conference
