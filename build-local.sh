e2e89526b1b72ad55740dee5fc5d6c708a9df128
Gennadiy Dubina <gdubina@dataart.com>
2015-11-02 17:43:34 +0200

    add ability to build/upload single app, update apps

--- build-local.sh
+++ build-local.sh
@@ -1,5 +1,12 @@
 VERSION=`cat images.version`
 
+if [ "$#" -eq 1 ]; then
+    APP=$1
+    echo "Build single app: $APP"
+    docker build -t tads2015da/$APP:$VERSION ./$APP
+    exit 0
+fi
+
 docker build -t tads2015da/calls-consumer:$VERSION ./calls-consumer
 docker build -t tads2015da/conference-call:$VERSION ./conference-call
 #ocker build -t tads2015da/demo-advertising:0.0.9 ./demo-advertising
--- upload-images.sh
+++ upload-images.sh
@@ -2,6 +2,13 @@
 
 VERSION=`cat images.version`
 
+if [ "$#" -eq 1 ]; then
+    APP=$1
+    echo "Upload single image: $APP"
+    docker push tads2015da/$APP:$VERSION
+    exit 0
+fi
+
 docker push tads2015da/drop-conference:$VERSION
 docker push tads2015da/conference-call:$VERSION
 docker push tads2015da/sms-feedback:$VERSION
